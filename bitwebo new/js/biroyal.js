f226af742f22989cf36c9d3fa3171d81b956b29a
Birupakshya Kar <beekay12345@gmail.com>
2020-05-14 13:14:40 +0530

    bitwebo.com new
    new website of bitwebo

---
+++ bitwebo new/js/biroyal.js
@@ -0,0 +1,44 @@
+$(".change").on('click', function(){
+	//alert("test");
+	$(".change").removeClass('active');
+	$(this).addClass('active');
+});
+function openModal(id)
+{
+	$("#"+id).modal();
+}
+
+
+function sendPartnerRequest(name,mobile_number,mail_id,message)
+{
+	if(name!="" && mobile_number!="" && mail_id!="" && message!="")
+	{
+		var data = { 
+						"name" : name,
+						"mobile" : mobile_number,
+						"email" : mail_id,
+						"message" : message
+					}
+		console.log(JSON.Stringify(data));
+		/*$.ajax({
+			url: "sendEmail.php",
+			method: "POST",
+			data: data,
+			success: function(response){
+				if(response.trim() == "success")
+				{
+					alert("Message sent successfully");
+				}
+				else{
+					alert("Error while sending message");
+				}
+				
+			}
+		});*/
+
+	}
+	else
+	{
+		alert("Please fill all values");
+	}
+}
